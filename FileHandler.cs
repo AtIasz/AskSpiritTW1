fc287b00a3d7a2263781a78462cd566511adef1c
Atlasz <lajos.kanalas@freemail.hu>
2020-03-18 11:01:29 +0100

    Added Question.cs,Answer.cs and FileHandler.cs

---
+++ FileHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AskSprint1_1.Views
+{
+    public class FileHandler
+    {
+        List<Question> questions;
+        List<Answer> answers;
+        public FileHandler()
+        {
+            questions = new List<Question>();
+            answers = new List<Answer>();
+            QuestionsReader();
+            AnswerReader();
+        }
+        private void QuestionsReader()
+        {
+            StreamReader sr = new StreamReader("questions.csv");
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                Question q = new Question(line);
+                questions.Add(q);
+            }
+        }
+        private void AnswerReader()
+        {
+            StreamReader sr = new StreamReader("answers.csv");
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                Answer a = new Answer(line);
+                answers.Add(a);
+            }
+        }
+    }
+}
---
+++ Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AskSprint1_1
+{
+    public class Question
+    {
+        string question { get; set; }
+        public Question(string question)
+        {
+            this.question = question;
+        }
+    }
+}
